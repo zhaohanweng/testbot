70a8299c49acaf729af3c48711e3aec8524bb999
Zhaohan Weng <wengzhaohan@gmail.com>
2020-12-23 01:20:22 -0500

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -1,1 +1,1 @@
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' {{secrets.ZW_SLACK_URL}}
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' $ZW_SLACK_URL
